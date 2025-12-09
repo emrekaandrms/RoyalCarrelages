054f8489d2e89fc10e19eac2e5e4d7eff284f651
emrekaandrms <54370499+emrekaandrms@users.noreply.github.com>
2025-12-09 14:00:33 +0300

    Fix dynamic export in carrelages route

--- app/carrelages/[slug]/page.tsx
+++ app/carrelages/[slug]/page.tsx
@@ -1,3 +1,3 @@
-export { default, generateMetadata, dynamic } from '../../tiles/[slug]/page';
-
-
+export { default, generateMetadata } from '../../tiles/[slug]/page';
+// Route config must be declared locally; Next.js does not allow re-exports.
+export const dynamic = 'force-dynamic';
